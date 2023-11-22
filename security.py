839c8ed2a6d364f5f94139a334cbb8eec58ae104
Deepraj4595 <151773148+Deepraj4595@users.noreply.github.com>
2023-11-22 23:43:42 +0530

    Update security.py

--- security.py
+++ security.py
@@ -7,12 +7,6 @@ def execute_command(user_input):
 
 user_input = input("Enter something: ")
 execute_command(user_input)
--------------------------------------------
-# .deepsource.toml
 
-[analysis]
-  [analysis.python]
-    [analysis.python.bandit]
-      enabled = true  # Enable Bandit security analysis
 
 
